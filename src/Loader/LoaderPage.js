47c4671a6501760f03480ac25493ed3eb3d84098
sofimelania <ssirnik1987@gmail.com>
2024-06-05 20:41:18 -0400

    add some files to the staging area

---
+++ src/Loader/LoaderPage.js
@@ -0,0 +1,11 @@
+import Loader from "./Loader";
+
+const LoaderPage = () => {
+    return (
+        <div className="container_loader">
+            <Loader />
+        </div>
+    );
+}
+
+export default LoaderPage;
